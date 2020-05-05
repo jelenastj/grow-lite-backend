675f53680727e6f0429f8e41f1a291542f318ab3
jelenastj <jelena.stjepanovic@gmail.com>
2020-05-05 15:31:42 -0500

    created all the tables

---
+++ db/migrate/20200505201850_create_veggies.rb
@@ -0,0 +1,14 @@
+class CreateVeggies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :veggies do |t|
+      t.string :name
+      t.text :description
+      t.string :maturity_time
+      t.string :sun_exposure
+      t.string :spread
+      t.string :season
+
+      t.timestamps
+    end
+  end
+end
