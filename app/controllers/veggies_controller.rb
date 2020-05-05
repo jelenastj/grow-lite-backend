1dde7a688aa1077d5b12057c38db89e01efcfbfc
jelenastj <jelena.stjepanovic@gmail.com>
2020-05-05 15:51:17 -0500

    added controlers

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -1,2 +1,6 @@
 class VeggiesController < ApplicationController
+    def index
+        veggies = Veggie.all
+        render json: veggies, except: [:created_at, :updated_at], include: :comments 
+    end
 end
