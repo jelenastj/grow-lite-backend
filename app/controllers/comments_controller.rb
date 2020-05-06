8bdd68d5e4c7f532abd549bbe5f3fc5e760331fc
Tr1ckC0 <trick.co@yahoo.com>
2020-05-06 16:21:04 -0500

    delete comment working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def destroy
         comment = Comment.find(params[:id])
+        comment.destroy
+    
         render json: {message: "comment deleted"}
     end
 
