d26ec2fa5353cbae70d5d0a6f078d0ac682cb429
Tr1ckC0 <trick.co@yahoo.com>
2020-05-06 11:12:16 -0500

    added show route to veggies controller

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -3,4 +3,10 @@ class VeggiesController < ApplicationController
         veggies = Veggie.all
         render json: veggies, except: [:created_at, :updated_at], include: :comments 
     end
+
+    def show
+        veggie = Veggie.find(params[:id])
+
+        render json: veggie, except: [:created_at, :updated_at], include: :comments 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :veggies, only: :index
+  resources :veggies, only: [:index, :show, :update]
   resources :users, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
