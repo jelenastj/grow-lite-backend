4e932640ee01c4f3c4a2f296fd9aa9eab6f70aa1
jelenastj <jelena.stjepanovic@gmail.com>
2020-05-07 16:15:55 -0500

    new seeds and images

--- db/migrate/20200505203026_create_comments.rb
+++ db/migrate/20200505203026_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.string     :username
       t.references :user, null: false, foreign_key: true
       t.references :veggie, null: false, foreign_key: true
       t.string :content
