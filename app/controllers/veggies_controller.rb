a43be88c6d9dbc5164689908ea9782fa34c1cc48
Tr1ckC0 <trick.co@yahoo.com>
2020-05-07 21:53:10 -0500

    added :username to comment_params

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -26,6 +26,6 @@ class VeggiesController < ApplicationController
         end
 
         def comment_params
-            params.require(:comment).permit(:user_id, :content)
+            params.require(:comment).permit(:user_id, :content, :username)
         end
 end
